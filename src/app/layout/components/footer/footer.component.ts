4b10a4fc4db853d861815ed8ab54262cd3c29a43
GustavoTejada <45311587+GustavoTejada@users.noreply.github.com>
2023-06-14 20:15:26 -0300

    cambios desafio3

--- src/app/layout/components/footer/footer.component.ts
+++ src/app/layout/components/footer/footer.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+interface User {
+  name: string,
+  lastname: string,
+  age: number,
+  address: Address
+}
+
+interface Address {
+  street: string,
+  number: number
+}
+
+interface RedSocial {
+  instagram: string,
+  facebook: string,
+  whatsapp: number
+}
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+
+ usuario: User = {
+  name: 'Gustavo',
+  lastname: 'Tejada',
+  age: 27,
+  address: {
+    street: 'Calle de ejemplo',
+    number: 1755
+  }
+ }
+
+ redesSociales: RedSocial = {
+  instagram: 'www.google.com',
+  facebook: 'etc',
+  whatsapp: 123123123123
+ }
 
 }
